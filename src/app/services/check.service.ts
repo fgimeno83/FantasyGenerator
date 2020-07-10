f21a320db9b96c2303aca76f38f78ac32a7c027f
Fernando Gimeno Peón <fernando.gp83@gmail.com>
2020-07-10 23:44:11 +0200

    Implement of magic treasure shop generator

--- src/app/services/check.service.ts
+++ src/app/services/check.service.ts
@@ -71,7 +71,7 @@ export class CheckService {
   //   return result;
   // }
 
-  private randomCheck(min: number, max: number) {
+  public randomCheck(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
--- src/app/services/generator.service.ts
+++ src/app/services/generator.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ConversorService } from './conversor.service';
 import { CheckService } from './check.service';
 import { TreasureFormModel } from '../model/treasure-form.model';
+import { element } from 'protractor';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,18 @@ export class GeneratorService {
   private conversorService: ConversorService;
 
   private checkService: CheckService;
+
+  private magicItemShopMap = [
+    {id: 1, value: '1d6:A'},
+    {id: 2, value: '1d3:B'},
+    {id: 3, value: '1d4:C'},
+    {id: 4, value: '1d4:D'},
+    {id: 5, value: '1d4:E'},
+    {id: 6, value: '1d4:F'},
+    {id: 7, value: '1d4:G'},
+    {id: 8, value: '1d4:H'},
+    {id: 9, value: '1d4:I'}
+  ];
 
   constructor(conversorService: ConversorService, checkService: CheckService) {
     this.conversorService = conversorService;
@@ -71,5 +84,31 @@ export class GeneratorService {
       itemList
     };
     return result;
+  }
+
+  public generateMagicShop(weeks: number, gold: number, isStatic: boolean, selectionId: number) {
+    const totalValue = 0;
+    let itemShopId = selectionId;
+    if (!isStatic) {
+      this.checkService.randomCheck(1, 20) + this.generateBonusCheck(weeks, gold);
+    }
+
+    const itemList = this.checkForMagiShopTreasure(itemShopId);
+
+    const result = {
+      totalValue,
+      itemList
+    };
+    return result;
+  }
+
+  private generateBonusCheck(weeks: number, gold: number) {
+    const goldBonus = gold / 100;
+    return (weeks - 1) + (goldBonus.toFixed() - 1);
+  }
+
+  private checkForMagiShopTreasure(id: number) {
+    const itemShopResult = this.magicItemShopMap.filter(itemShopElement => itemShopElement.id = id).map(item => item.value);
+    return this.checkService.checkMagicItem(itemShopResult[0]);
   }
 }
