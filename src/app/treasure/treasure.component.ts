312716bfcdd61cb09c8b9a03986276c9289ff56e
Fernando Gimeno Peón <fernando.gp83@gmail.com>
2020-07-10 19:58:39 +0200

    Implement of magic shop generator

--- src/app/treasure/treasure.component.ts
+++ src/app/treasure/treasure.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { GeneratorService } from '../services/generator.service';
 
 @Component({
   selector: 'app-treasure',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TreasureComponent implements OnInit {
 
-  constructor() { }
+  private formBuilder: FormBuilder;
+  private generatorService: GeneratorService;
+  public options: FormGroup;
+  public checks = [
+    {name: '1-5'},
+    {name: '6-10'},
+    {name: '11-15'},
+    {name: '16-20'},
+    {name: '21-25'},
+    {name: '26-30'},
+    {name: '31-35'},
+    {name: '36-40'},
+    {name: '41+'}
+  ];
 
+  constructor(generatorService: GeneratorService, formBuilder: FormBuilder) {
+    this.generatorService = generatorService;
+    this.formBuilder = formBuilder;
+  }
+
+  //+1 semana, +1 100po, max +10, percepcion
   ngOnInit(): void {
   }
 
+  public send() {
+
+  }
 }
